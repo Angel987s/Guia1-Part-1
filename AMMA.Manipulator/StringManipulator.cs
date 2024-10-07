9b031b8d33065dd4869e1388840105164d6c274c
Angel Mena <angelmeba4@gmail.com>
2024-10-07 10:38:21 -0600

    Agregar archivos de proyecto.

---
+++ AMMA.Manipulator/StringManipulator.cs
@@ -0,0 +1,21 @@
+﻿namespace AMMA.Manipulator
+{
+    public class StringManipulator
+    {
+        public string ReverseString (string input)
+        {
+            if (input == null)
+                return null;
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+        public string RemoveSpaces (string input) {
+            if (input == null)
+                return null;
+
+            return input.Replace(" ", string.Empty);
+        }
+    }
+}
